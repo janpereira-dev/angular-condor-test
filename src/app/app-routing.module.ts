92f8fb23dcb761aaf479e6aae1e3f882fb2e1969
Jan Pereira <jan.pereira@outlook.com>
2023-04-13 16:49:54 +0200

    feat(form): new components home and form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'form', loadChildren: () => import('./pages/form/form.module').then(m => m.FormModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
